e44a14b9479ee28f9cb40cebb242ac075ae196c8
SATOSHI NAKAMOTO <satoshinakamoto@SATOSHIs-MBP.cablelynx.com>
2024-04-02 23:58:35 -0500

    Add basic HomePage component

---
+++ components/HomePage.js
@@ -0,0 +1,13 @@
+// HomePage.js
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Therapy Buddy!</h1>
+      <p>Your mental health companion.</p>
+    </div>
+  );
+}
+
+export default HomePage;
