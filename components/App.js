cd1afad055b6f5e874027e1670b69b99aea242ed
SATOSHI NAKAMOTO <satoshinakamoto@SATOSHIs-MBP.cablelynx.com>
2024-04-03 04:18:36 -0500

    Trying once more

--- components/App.js
+++ components/App.js
@@ -1,19 +1,12 @@
-// App.js
-import React from 'react'; //  Always needed for React components
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Import for routing
-import HomePage from './components/HomePage';
-import TherapyAI from './components/TherapyAI'; 
-import Journal from './components/Journal'; // Assuming you'll create this component later
-import MoodTracker from './components/MoodTracker'; // Assuming you'll create this later
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage'; 
 
 function App() {
   return (
-    <BrowserRouter> {/* Wrap your app with the browser router */}
-      <Routes> {/* Define your routes here */}
+    <BrowserRouter> 
+      <Routes> 
          <Route path="/" element={<HomePage />} /> 
-         <Route path="/therapy-ai" element={<TherapyAI />} />
-         <Route path="/journal" element={<Journal />} />  
-         <Route path="/mood-tracker" element={<MoodTracker />} /> 
       </Routes>
     </BrowserRouter>
   );
--- components/HomePage.js
+++ components/HomePage.js
@@ -1,22 +1,9 @@
-// components/HomePage.js
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 function HomePage() {
-  return (
-    <div className="homepage-container">
-      <h1>Welcome to Therapy Buddy</h1>
-      <p>Your Mental Health Companion</p>
-      <nav>
-        <ul>
-          <li><Link to="/therapy-ai">Chat with Therapy AI</Link></li>
-          <li><Link to="/journal">Journal</Link></li> 
-          <li><Link to="/mood-tracker">Mood Tracker</Link></li> 
-          {/* Add more links as you create new pages */}
-        </ul>
-      </nav>
-    </div>
-  );
+    return ( 
+        <div>Hello World</div>
+   );
 }
 
-export default HomePage;
+export default HomePage; 
